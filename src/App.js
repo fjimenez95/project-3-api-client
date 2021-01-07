4c8ffa1de655dd299c024da67c969dd6bc68f376
Freddy Jimenez <freddy.junior@gmail.com>
2021-01-06 19:26:09 -0500

    imports header and footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Footer from './components/Footer/Footer';
+// YOU CAN CHANGE THE FOOTER / HEADER FILES TO INDEX.JS AND YOU DON'T HAVE TO DO THE FINAL PREFIX /FOOTER BECAUSE SYSTEM WILL ALREADY KNOW TO LOOK FOR INDEX.JS
+import Header from './components/Header/Header';
+
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <p>Project 3</p>
+      <Footer />
     </div>
   );
 }
